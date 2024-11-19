6f9ddd57b6fd1503bda7f581d0bd238a7aeec0e1
Brandon Riley <Boriley@email.sc.edu>
2024-11-18 23:49:41 -0500

    Added PrivacySetting Model

--- prototype/BojanglesSecurity/models.py
+++ prototype/BojanglesSecurity/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class PrivacySettings(models.Model):
+    EnableDataCollection = models.BooleanField(default=True)
+    ActivityTracking = models.BooleanField(default=True)
+    VoiceDataCollection = models.BooleanField(default=True)
+    LocationTracking = models.BooleanField(default=True)
+    ShareDataWithThirdParties = models.BooleanField(default=True)
+    AllowUsageTracking = models.BooleanField(default=True)
+    DataEncryption = models.BooleanField(default=True)
+    StorePersonalUsageData = models.BooleanField(default=True)
+    StorePurchaseHistory = models.BooleanField(default=True)
+    AllowDataCollectionForCrossDevice = models.BooleanField(default=True)
